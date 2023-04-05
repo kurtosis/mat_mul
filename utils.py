9eb7afc64bc9d54d68bcc1e400109fc2be2b05da
Kurt Smith <smith.kurt.a@gmail.com>
2023-04-05 00:21:08 -0400

    add get_scalars

--- utils.py
+++ utils.py
@@ -1,3 +1,16 @@
+import torch
+
+
+def get_scalars(tt: torch.Tensor, t_step: int, batch_size = True):
+    if batch_size:
+        b = tt.shape[0]
+        scalars = torch.zeros((b, 1))
+        scalars[:, 0] = t_step
+    else:
+        scalars = torch.tensor(t_step).unsqueeze(-1).float()
+    return scalars
+
+
 def print_params(alpha):
     print(f"{sum(p.numel() for p in alpha.parameters()) // int(1e6)}M parameters")
     print(f"{sum(p.numel() for p in alpha.parameters()) // int(1e3)}k parameters")
